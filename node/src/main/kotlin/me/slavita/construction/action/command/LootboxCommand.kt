a261ea3786d3870c4e91a0209cb9baed8261a16e
RomanAndr <andrroman07@gmail.com>
2022-09-30 23:46:16 +0300

    Selections refactor

--- node/src/main/kotlin/me/slavita/construction/action/command/LootboxCommand.kt
+++ node/src/main/kotlin/me/slavita/construction/action/command/LootboxCommand.kt
@@ -4,8 +4,8 @@ import me.func.mod.conversation.data.LootDrop
 import me.func.mod.reactive.ReactiveButton
 import me.func.mod.ui.menu.Openable
 import me.func.mod.ui.menu.selection.Selection
-import me.slavita.construction.app
 import me.slavita.construction.action.MenuCommand
+import me.slavita.construction.app
 import me.slavita.construction.ui.ItemIcons
 import me.slavita.construction.worker.WorkerGenerator
 import me.slavita.construction.worker.WorkerRarity
@@ -14,36 +14,34 @@ import org.bukkit.entity.Player
 class LootboxCommand(player: Player) : MenuCommand(player) {
 
     override fun getMenu(): Openable {
-        val storage = mutableListOf<ReactiveButton>()
-
-        app.getUser(player).run {
-            WorkerRarity.values().forEach {
-                val canBuy = stats.money >= it.price
-                val iconItem = ItemIcons.get(it.iconKey, it.iconValue, it.iconMaterial)
-
-                storage.add(
-                    ReactiveButton()
-                        .item(iconItem)
-                        .title(it.title)
-                        .description(it.description)
-                        .price(it.price)
-                        .hint(if (canBuy) "Купить" else "Недостаточно\nсредств")
-                        .onClick { _, _, _ ->
-                            if (!canBuy) return@onClick
-
-                            val worker = WorkerGenerator.generate(it)
-                            val lootDrop = LootDrop(iconItem, worker.name, it.dropRare)
-                            OpenWorker(this, worker, lootDrop).tryExecute()
-                        }
-                )
-            }
-
+        app.getUser(player).run user@ {
             return Selection(
                 title = "Покупка строителей",
                 money = "Ваш баланс ${stats.money}",
                 rows = 3,
                 columns = 3,
-                storage = storage
+                storage = mutableListOf<ReactiveButton>().apply storage@ {
+                    WorkerRarity.values().forEach { rarity ->
+                        val canBuy = stats.money >= rarity.price
+                        val iconItem = ItemIcons.get(rarity.iconKey, rarity.iconValue, rarity.iconMaterial)
+
+                        this@storage.add(
+                            ReactiveButton()
+                                .item(iconItem)
+                                .title(rarity.title)
+                                .description(rarity.description)
+                                .price(rarity.price)
+                                .hint(if (canBuy) "Купить" else "Недостаточно\nсредств")
+                                .onClick { _, _, _ ->
+                                    if (!canBuy) return@onClick
+
+                                    val worker = WorkerGenerator.generate(rarity)
+                                    val lootDrop = LootDrop(iconItem, worker.name, rarity.dropRare)
+                                    OpenWorker(this@user, worker, lootDrop).tryExecute()
+                                }
+                        )
+                    }
+                }
             )
         }
     }
