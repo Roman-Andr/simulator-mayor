63768d577a75dc21cf7e0ca70ca35537e326c567
slavita <vitalik_ermakov_228@mail.ru>
2022-10-18 20:45:14 +0300

    maybe optimize

--- node/src/main/kotlin/me/slavita/construction/structure/BuildingStructure.kt
+++ node/src/main/kotlin/me/slavita/construction/structure/BuildingStructure.kt
@@ -89,6 +89,10 @@ abstract class BuildingStructure(
         deleteVisual()
         visual.finishShow()
         owner.city.cityStructures.add(CityStructure(owner.player, structure, cell))
+
+        box.getChunks().forEach {chunk ->
+            world.blocks[owner.player.uniqueId]?.get(chunk)?.removeIf { box.contains(it.position) }
+        }
     }
 
     fun claimed() {
