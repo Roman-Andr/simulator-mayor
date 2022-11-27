9d080dc55b9048b0576a453e7adcb2ff89bcab94
RomanAndr <andrroman07@gmail.com>
2022-11-27 18:19:55 +0300

    fix storage

--- node/src/main/kotlin/me/slavita/construction/action/command/menu/storage/StorageMenu.kt
+++ node/src/main/kotlin/me/slavita/construction/action/command/menu/storage/StorageMenu.kt
@@ -14,21 +14,21 @@ class StorageMenu(player: Player) : MenuCommand(player) {
     override fun getMenu(): Openable {
         return getBaseSelection(MenuInfo("${GREEN}${BOLD}Склад", StatsType.LEVEL, 5, 14)).apply {
             storage = mutableListOf<ReactiveButton>().apply {
-                val blocksStorage = player.user.blocksStorage
-                storage = blocksStorage.blocks.map {
-                    button {
+                val storage = player.user.blocksStorage
+                storage.blocks.forEach {
+                    add(button {
+                        item = it.value
                         hover = """
                             На складе ${it.value.amount} шт
                             Взять ${if (it.value.amount >= 32) 32 else it.value.amount} шт
                         """.trimIndent()
                         hint = " "
-                        item = it.value
                         onClick { _, _, _ ->
-                            player.inventory.addItem(it.key.createItemStack(blocksStorage.removeItem(it.value, 32)))
+                            player.inventory.addItem(it.key.createItemStack(storage.removeItem(it.value, 32)))
                             StorageMenu(player).tryExecute()
                         }
-                    }
-                }.toMutableList()
+                    })
+                }
             }
         }
     }
